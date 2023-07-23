530fce372841b587a8e18a33a982c0d04e394ec2
adila <adila@DESKTOP-VEV4J0V>
2023-07-23 19:29:45 +0500

    Add project files.

---
+++ JobAPI/Controllers/GetCandidateResultStatusController.cs
@@ -0,0 +1,70 @@
+﻿using JobAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using static JobAPI.Models.CandidateResultStatusModel;
+
+namespace JobAPI.Controllers
+{
+    public class GetCandidateResultStatusController : ApiController
+    {
+        FYP_TEST_DBEntities dx = new FYP_TEST_DBEntities();
+
+        public CandidateResultStatusModel GetCandidateResultStatus()
+        {
+            CandidateResultStatusModel rply = new CandidateResultStatusModel();
+            try
+            {
+                List<CandidateResultStatusDetail> lst = new List<CandidateResultStatusDetail>();
+
+                var query = (dx.sp_GetCandidateResultStatus()).ToList();
+                if (query.ToList().Count > 0)
+                {
+                    foreach (var x in query)
+                    {
+                        lst.Add(new CandidateResultStatusDetail
+                        {
+                            CandidateID = x.CandidateID.ToString(),
+                            JObID = x.JObID.ToString(),
+                            CandidateName = x.CandidateName,
+                            CandidateContact = x.CandidateContact,
+                            CandidateCNIC = x.CnadidateCNIC,
+                            CandidateEmail = x.CandidateEmail,
+                            ResultStatus = x.ResultStatus,
+                            JobTitle = x.JobTitle,
+                            PositionName = x.PositionName,
+                            JobType = x.JobType,
+                            DepartmentName = x.DepartmentName,
+                            Experience = x.Experience.ToString(),
+                            City = x.City,
+                        });
+
+                    }
+
+                    rply.Response = "success";
+                    rply.ErrorDescription = "";
+                    rply.detail = lst;
+                }
+                else
+                {
+                    rply.Response = "failure";
+                    rply.ErrorDescription = "No Record Found";
+                }
+
+                return rply;
+
+            }
+            catch (Exception ex)
+            {
+                rply.Response = "failure";
+                rply.ErrorDescription = ex.Message;
+                return rply;
+
+            }
+
+        }
+    }
+}
---
+++ JobAPI/Models/CandidateResultStatusModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobAPI.Models
+{
+    public class CandidateResultStatusModel
+    {
+        String response;
+        public String Response
+        {
+            get { return response; }
+            set { response = value; }
+        }
+
+        String errorDescription;
+        public String ErrorDescription
+        {
+            get { return errorDescription; }
+            set { errorDescription = value; }
+        }
+
+        List<CandidateResultStatusDetail> Detail;
+        public List<CandidateResultStatusDetail> detail
+        {
+            get { return Detail; }
+            set { Detail = value; }
+        }
+
+        public class CandidateResultStatusDetail
+        {
+            public string CandidateID { get; set; }
+            public string JObID { get; set; }
+            public string CandidateName { get; set; }
+            public string CandidateContact { get; set; }
+            public string CandidateCNIC { get; set; }
+            public string CandidateEmail { get; set; }
+            public string ResultStatus { get; set; }
+            public string JobTitle { get; set; }
+            public string PositionName { get; set; }
+            public string JobType { get; set; }
+            public string DepartmentName { get; set; }
+            public string Experience { get; set; }
+            public string City { get; set; }
+
+
+
+        }
+    }
+}
